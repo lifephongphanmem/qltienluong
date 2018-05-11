3927e4bd44b5028e423979e66f13ff662dfc9027
HuongVu <vuhuong297@mail.com>
2018-05-11 16:34:02 +0700

    update sửa lương + nghỉ thai sản

--- app/Http/Controllers/xemdulieucapduoiController.php
+++ app/Http/Controllers/xemdulieucapduoiController.php
@@ -88,7 +88,7 @@ class xemdulieucapduoiController extends Controller
                     ->where('trangthai', 'DAGUI')
                     ->first();
 
-                if (isset($model_bangluong)) {
+                if (count($model_bangluong)>0) {
                     $dv->mathdv = $model_bangluong->mathdv;
                     $model_bangluong_ct = tonghopluong_donvi_chitiet::where('mathdv', $dv->mathdv)->first();
                     $dv->tralai = count($model_bangluong_ct->mathk) > 0 ? false : true;
