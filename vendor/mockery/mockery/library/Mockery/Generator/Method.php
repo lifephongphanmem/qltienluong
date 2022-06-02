8dc68283ff55dd19d9e556775fdd7258a0b43b8b
lifephongphanmem <61262850+lifephongphanmem@users.noreply.github.com>
2022-06-02 11:24:03 +0700

    update php7

--- vendor/mockery/mockery/library/Mockery/Generator/Method.php
+++ vendor/mockery/mockery/library/Mockery/Generator/Method.php
@@ -1,9 +1,30 @@
 <?php
+/**
+ * Mockery
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://github.com/padraic/mockery/blob/master/LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic@php.net so we can send you a copy immediately.
+ *
+ * @category   Mockery
+ * @package    Mockery
+ * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
+ */
 
 namespace Mockery\Generator;
 
+use Mockery\Reflector;
+
 class Method
 {
+    /** @var \ReflectionMethod */
     private $method;
 
     public function __construct(\ReflectionMethod $method)
@@ -16,18 +37,21 @@ class Method
         return call_user_func_array(array($this->method, $method), $args);
     }
 
+    /**
+     * @return Parameter[]
+     */
     public function getParameters()
     {
-        return array_map(function ($parameter) {
+        return array_map(function (\ReflectionParameter $parameter) {
             return new Parameter($parameter);
         }, $this->method->getParameters());
     }
 
+    /**
+     * @return string|null
+     */
     public function getReturnType()
     {
-        if (version_compare(PHP_VERSION, '7.0.0-dev') >= 0 && $this->method->hasReturnType()) {
-            return (string) $this->method->getReturnType();
-        }
-        return '';
+        return Reflector::getReturnType($this->method);
     }
 }
