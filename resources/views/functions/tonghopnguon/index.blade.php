f8b07cb6688207c47d199649aae44bc53490b1b8
lifephongphanmem <61262850+lifephongphanmem@users.noreply.github.com>
2022-10-17 19:02:50 +0700

    gửi so lieu

--- resources/views/functions/tonghopnguon/index.blade.php
+++ resources/views/functions/tonghopnguon/index.blade.php
@@ -5,20 +5,23 @@
  * Date: 24/06/2016
  * Time: 4:00 PM
  */
-        ?>
+?>
 @extends('main')
 
 @section('custom-style')
-    <link rel="stylesheet" type="text/css" href="{{url('assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css')}}"/>
-    <link rel="stylesheet" type="text/css" href="{{url('assets/global/plugins/select2/select2.css')}}"/>
+    <link rel="stylesheet" type="text/css"
+        href="{{ url('assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/global/plugins/select2/select2.css') }}" />
 @stop
 
 @section('custom-script')
-    <script type="text/javascript" src="{{url('assets/global/plugins/select2/select2.min.js')}}"></script>
-    <script type="text/javascript" src="{{url('assets/global/plugins/datatables/media/js/jquery.dataTables.min.js')}}"></script>
-    <script type="text/javascript" src="{{url('assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js')}}"></script>
+    <script type="text/javascript" src="{{ url('assets/global/plugins/select2/select2.min.js') }}"></script>
+    <script type="text/javascript" src="{{ url('assets/global/plugins/datatables/media/js/jquery.dataTables.min.js') }}">
+    </script>
+    <script type="text/javascript"
+        src="{{ url('assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js') }}"></script>
 
-    <script src="{{url('assets/admin/pages/scripts/table-managed.js')}}"></script>
+    <script src="{{ url('assets/admin/pages/scripts/table-managed.js') }}"></script>
     <script>
         jQuery(document).ready(function() {
             TableManaged.init();
@@ -37,46 +40,57 @@
 
                     <table id="sample_3" class="table table-hover table-striped table-bordered" style="min-height: 230px">
                         <thead>
-                        <tr>
-                            <th class="text-center" style="width: 5%">STT</th>
-                            <th class="text-center">Căn cứ thông tư, quyết định</th>
-                            <th class="text-center">Đơn vị gửi</br>số liệu</th>
-                            <th class="text-center">Trạng thái</th>
-                            <th class="text-center">Thao tác</th>
-                        </tr>
+                            <tr>
+                                <th class="text-center" style="width: 5%">STT</th>
+                                <th class="text-center">Căn cứ thông tư, quyết định</th>
+                                <th class="text-center">Đơn vị gửi</br>số liệu</th>
+                                <th class="text-center">Trạng thái</th>
+                                <th class="text-center">Thao tác</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        @if(isset($model))
-                            @foreach($model as $key=>$value)
-                                <tr>
-                                    <td class="text-center">{{$key+1}}</td>
-                                    <td>{{$value->tenttqd}}</td>
-                                    <td class="text-center">{{$value->sldv}}</td>
-                                    <td class="text-center bold">{{$a_trangthai[$value['trangthai']]}}</td>
-                                    <td>
-                                        <a href="{{url($furl_th.'tonghop?sohieu='.$value->sohieu)}}" class="btn btn-default btn-xs" target="_blank">
-                                            <i class="fa fa-print"></i>&nbsp; Số liệu tổng hợp</a>
-                                            @if($value['trangthai'] == 'CHUAGUI' || $value['trangthai'] == 'TRALAI')
+                            @if (isset($model))
+                                @foreach ($model as $key => $value)
+                                    <tr>
+                                        <td class="text-center">{{ $key + 1 }}</td>
+                                        <td>{{ $value->tenttqd }}</td>
+                                        <td class="text-center">{{ $value->sldv }}</td>
+                                        <td class="text-center bold">{{ $a_trangthai[$value['trangthai']] }}</td>
+                                        <td>
+                                            <a href="{{ url($furl_th . 'tonghop?sohieu=' . $value->sohieu) }}"
+                                                class="btn btn-default btn-xs" target="_blank">
+                                                <i class="fa fa-print"></i>&nbsp; Số liệu tổng hợp</a>
+                                            @if ($value['trangthai'] != 'DAGUI')
+                                                <button type="button" class="btn btn-default btn-xs"
+                                                    onclick="confirmChuyen('{{ $value->sohieu }}')"
+                                                    data-target="#chuyen-modal" data-toggle="modal"><i
+                                                        class="fa fa-share-square-o"></i>&nbsp;
+                                                    Gửi dữ liệu</button>
+                                            @endif
+                                            {{-- @if ($value['trangthai'] == 'CHUAGUI' || $value['trangthai'] == 'TRALAI')
                                                 <button type="button" class="btn btn-default btn-xs" onclick="confirmChuyen('{{$value->sohieu}}')" data-target="#chuyen-modal" data-toggle="modal"><i class="fa fa-share-square-o"></i>&nbsp;
                                                     Gửi dữ liệu</button>
                                             @else
                                                 <button disabled type="button" class="btn btn-default btn-xs" onclick="confirmChuyen('{{$value->sohieu}}')" data-target="#chuyen-modal" data-toggle="modal"><i class="fa fa-share-square-o"></i>&nbsp;
                                                     Gửi dữ liệu</button>
+                                            @endif --}}
+
+                                            @if ($value['trangthai'] == 'TRALAI')
+                                                <button type="button" class="btn btn-default btn-xs"
+                                                    onclick="getLyDo('{{ $value->masodv }}','{{ $value->sohieu }}')"
+                                                    data-target="#tralai-modal" data-toggle="modal"><i
+                                                        class="fa fa-share-square-o"></i>&nbsp;
+                                                    Lý do trả lại</button>
                                             @endif
 
-                                        @if($value['trangthai'] == 'TRALAI')
-                                            <button type="button" class="btn btn-default btn-xs" onclick="getLyDo('{{$value->masodv}}','{{$value->sohieu}}')" data-target="#tralai-modal" data-toggle="modal"><i class="fa fa-share-square-o"></i>&nbsp;
-                                                Lý do trả lại</button>
+                                            <a href="{{ url($furl_xem . '?sohieu=' . $value->sohieu . '&trangthai=ALL&phanloai=ALL') }}"
+                                                class="btn btn-default btn-xs">
+                                                <i class="fa fa-list-alt"></i>&nbsp; Số liệu chi tiết</a>
+                                        </td>
 
-                                        @endif
-
-                                        <a href="{{url($furl_xem.'?sohieu='.$value->sohieu.'&trangthai=ALL&phanloai=ALL')}}" class="btn btn-default btn-xs">
-                                            <i class="fa fa-list-alt"></i>&nbsp; Số liệu chi tiết</a>
-                                    </td>
-
-                                </tr>
-                            @endforeach
-                        @endif
+                                    </tr>
+                                @endforeach
+                            @endif
                         </tbody>
                     </table>
                 </div>
@@ -85,17 +99,19 @@
     </div>
 
     <!--Model chuyển-->
-    <div class="modal fade" id="chuyen-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal fade" id="chuyen-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                {!! Form::open(['url'=>$furl_th.'senddata','id' => 'frm_chuyen','method'=>'POST'])!!}
+                {!! Form::open(['url' => $furl_th . 'senddata', 'id' => 'frm_chuyen', 'method' => 'POST']) !!}
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
                     <h4 class="modal-title">Đồng ý chuyển số liệu?</h4>
                 </div>
                 <div class="modal-body">
                     <div class="form-group">
-                        <label><b>Số liệu tổng hợp khi gửi đi sẽ không thể chỉnh sửa. Bạn hãy kiểm tra kỹ số liệu trước khi gửi.</b></label>
+                        <label><b>Số liệu tổng hợp khi gửi đi sẽ không thể chỉnh sửa. Bạn hãy kiểm tra kỹ số liệu trước khi
+                                gửi.</b></label>
                     </div>
                     <input type="hidden" name="sohieu" id="sohieu">
                     <div class="modal-footer">
@@ -111,40 +127,42 @@
         </div>
     </div>
 
-            <!--Model Trả lại -->
-            <div class="modal fade" id="tralai-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-        
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-                            <h4 class="modal-title">Thông tin lý do trả lại dữ liệu</h4>
-                        </div>
-                        <div class="modal-body">
-                            <div class="form-group">
-                                {!!Form::textarea('lydo', null, array('id' => 'lydo','class' => 'form-control','rows'=>'3'))!!}
-                            </div>
-        
-                            <div class="modal-footer">
-                                <button type="submit" class="btn blue">Đồng ý</button>
-        
-                            </div>
-                            {!! Form::close() !!}
-                        </div>
-                        <!-- /.modal-content -->
-                    </div>
-                    <!-- /.modal-dialog -->
+    <!--Model Trả lại -->
+    <div class="modal fade" id="tralai-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <h4 class="modal-title">Thông tin lý do trả lại dữ liệu</h4>
                 </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        {!! Form::textarea('lydo', null, ['id' => 'lydo', 'class' => 'form-control', 'rows' => '3']) !!}
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="submit" class="btn blue">Đồng ý</button>
+
+                    </div>
+                    {!! Form::close() !!}
+                </div>
+                <!-- /.modal-content -->
             </div>
+            <!-- /.modal-dialog -->
+        </div>
+    </div>
 
     <script>
         function confirmChuyen(sohieu) {
             document.getElementById("sohieu").value = sohieu;
         }
-        function getLyDo(masodv,sohieu){
+
+        function getLyDo(masodv, sohieu) {
             var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
             $.ajax({
-                url: '{{$furl_th}}' + 'getlydo',
+                url: '{{ $furl_th }}' + 'getlydo',
                 type: 'GET',
                 data: {
                     _token: CSRF_TOKEN,
@@ -152,11 +170,11 @@
                     sohieu: sohieu
                 },
                 dataType: 'JSON',
-                success: function (data) {
+                success: function(data) {
                     $('#lydo').val(data.lydo);
                 },
-                error: function(message){
-                    toastr.error(message,'Lỗi!');
+                error: function(message) {
+                    toastr.error(message, 'Lỗi!');
                 }
             });
 
