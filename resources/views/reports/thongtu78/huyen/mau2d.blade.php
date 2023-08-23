81901f0df156136bcdf5966fa3f2f88387727ca4
Nguyen Duy Dung <dunght38@gmail.com>
2023-08-23 10:02:17 +0700

    sua mau 2d huyen

--- resources/views/reports/thongtu78/huyen/mau2d.blade.php
+++ resources/views/reports/thongtu78/huyen/mau2d.blade.php
@@ -91,7 +91,8 @@
                 <td>{{ dinhdangsothapphan($dulieu['solieu']['tyledonggop_2d'], $inputs['lamtron']) }}</td>
                 <td>{{ dinhdangsothapphan($dulieu['solieu']['soluongdinhbien_2d'], $inputs['lamtron']) }}</td>
                 <td>{{ dinhdangsothapphan($dulieu['solieu']['soluongdonvi_2k']*$dulieu['solieu']['soluongdinhbien_2d'], $inputs['lamtron']) }}</td>
-                <td>{{ dinhdangsothapphan($dulieu['solieu']['tongqd34_2d'] - $dulieu['solieu']['soluongdinhbien_2d'], $inputs['lamtron']) }}
+                {{-- <td>{{ dinhdangsothapphan($dulieu['solieu']['tongqd34_2d'] - $dulieu['solieu']['soluongdinhbien_2d'], $inputs['lamtron']) }} --}}
+                    <td>{{ dinhdangsothapphan($dulieu['solieu']['tongqd34_2d'] - ($dulieu['solieu']['soluongdonvi_2k']*$dulieu['solieu']['soluongdinhbien_2d']), $inputs['lamtron']) }}
                 </td>
                
                 <td>{{ dinhdangsothapphan($dulieu['solieu']['quyluonggiam_2k'], $inputs['lamtron']) }}</td>
