c37c7db34efa24b80310252be29c3fb522c37331
Nguyen Duy Dung <dunght38@gmail.com>
2024-12-19 10:07:09 +0700

    chinh sua thiet lap donvibaocao

--- app/Http/Controllers/dmphanloaidonvi_baocaoController.php
+++ app/Http/Controllers/dmphanloaidonvi_baocaoController.php
@@ -23,8 +23,10 @@ class dmphanloaidonvi_baocaoController extends Controller
             $inputs = $request->all();
             $inputs['madvbc'] = $inputs['madvbc'] ?? session('admin')->madvbc;
             $model = dmphanloaidonvi_baocao::where('madvbc', session('admin')->madvbc)->get();
+            // dd($model);
             $a_phanloai = array_column(dmphanloaidonvi::wherenotin('maphanloai', array_column($model->toarray(), 'maphanloai_nhom'))->get()->toArray(), 'tenphanloai', 'maphanloai');
             $a_goc = array_column(dmphanloaidonvi_baocao::wherenotin('maphanloai_nhom', array_keys($a_phanloai))->get()->toArray(), 'tenphanloai_nhom', 'maphanloai_nhom');
+            $a_goc[''] = 'Chọn mã phân loại gốc';
             return view('system.danhmuc.baocao.index')
                 ->with('model', $model)
                 ->with('a_phanloai', $a_phanloai)
@@ -82,6 +84,7 @@ class dmphanloaidonvi_baocaoController extends Controller
             $inputs = $request->all();
             $inputs['madvbc'] = session('admin')->madvbc;
             $model = dmphanloaidonvi_baocao::where('madvbc', $inputs['madvbc'])->where('maphanloai_nhom', $inputs['maphanloai_nhom'])->first();
+            // dd($inputs);
             //Kiểm tra nếu mã nhóm trùng với mã trong phân loại đơn vị thì báo lỗi
             $chk = dmphanloaidonvi::where('maphanloai', $inputs['maphanloai_goc'])->get();
             if ($chk->count() > 0) {
